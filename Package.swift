c182b995eabada012684e2128521cb2ce2c54d7d
Abhi <abeemukthees@gmail.com>
2021-07-11 22:58:02 +0530

    Setup Swift Package Manager
    Change name to lowercase

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Dryrun",
+    name: "dryrun",
     platforms: [.iOS("11"), .watchOS("4"), .tvOS("11"), .macOS("10.13"),],
     products: [
         .library(
