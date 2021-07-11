14c29dd4ced298258d771412d2c045a94c484efa
Abhi <abeemukthees@gmail.com>
2021-07-11 22:34:30 +0530

    Setup Swift Package Manager
    Fix Package.swift

--- dryrun/build.gradle.kts
+++ dryrun/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
-    //id("com.chromaticnoise.multiplatform-swiftpackage") version "2.0.3"
+    id("com.chromaticnoise.multiplatform-swiftpackage") version "2.0.3"
     //id("com.prof18.kmp.fatframework.cocoa") version "0.2.1"
     id("com.android.library")
     id("org.jetbrains.dokka") version Versions.dokka
@@ -302,5 +302,23 @@ fun Task.buildXcFramework(frameworks: List<org.jetbrains.kotlin.gradle.plugin.mp
     exec {
         executable = "xcodebuild"
         args = buildArgs()
+    }
+}
+
+//----------------------------------------------------------------------------------
+
+multiplatformSwiftPackage {
+    swiftToolsVersion("5.3")
+    packageName(project.name)
+    outputDirectory(File(projectDir, "swiftpackage"))
+    targetPlatforms {
+        iOS { v("5") }
+        tvOS { v("9") }
+        macOS { v("10") }
+        //watchOS { v("4") }
+        //targets("watchosArm64") { v("4") }
+        //targets("watchosX86") { v("4") }
+        //targets("watchosX64") { v("4") }
+
     }
 }
